89121031785652b36612731d2e92e8318e6a98dd
Liu Qiaoyu <1184893257@qq.com>
2012-11-02 20:37:47 +0800

    CS(0.4):添加客户端的组件

--- test/CS/Client/Resource.h
+++ test/CS/Client/Resource.h
@@ -8,7 +8,15 @@
 #define IDD_CLIENT_DIALOG               102
 #define IDP_SOCKETS_INIT_FAILED         103
 #define IDR_MAINFRAME                   128
-#define IDC_DATETIMEPICKER1             1002
+#define IDC_EDIT1                       1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT3                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON2                     1007
+#define IDC_BUTTON3                     1008
+#define IDC_BUTTON4                     1009
+#define IDC_LIST1                       1010
+#define IDC_EDIT4                       1011
 
 // Next default values for new objects
 // 
@@ -16,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
