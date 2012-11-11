042f503ec2dcdcb98378cd241163d1bd31c484b3
Liu Qiaoyu <1184893257@qq.com>
2012-11-11 22:08:27 +0800

    breakloop(0.2):添加下拉框, ID为 IDC_DEVS, 用于选择要监听的适配器

--- test/breakloop/Resource.h
+++ test/breakloop/Resource.h
@@ -1,21 +1,21 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by BREAKLOOP.RC
+// Microsoft Developer Studio generated include file.
+// Used by breakloop.rc
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_BREAKLOOP_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_BREAKLOOP_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_DEVS                        1000
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- test/breakloop/breakloop.clw
+++ test/breakloop/breakloop.clw
@@ -47,8 +47,6 @@ Control4=IDOK,button,1342373889
 [DLG:IDD_BREAKLOOP_DIALOG]
 Type=1
 Class=CBreakloopDlg
-ControlCount=3
-Control1=IDOK,button,1342242817
-Control2=IDCANCEL,button,1342242816
-Control3=IDC_STATIC,static,1342308352
+ControlCount=1
+Control1=IDC_DEVS,combobox,1344340226
 
